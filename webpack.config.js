5eec9d75da538d4286ea00f923ed54773f3417bd
thefx3 <fx.pairault@yahoo.fr>
2025-05-30 00:48:08 +0200

    Basic files

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+// webpack.config.js
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development", // ou "production"
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  devtool: "eval-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html", // Important : bon chemin
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+};
