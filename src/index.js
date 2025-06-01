108262f10b29e680193fb0ca13d0a6f2b27f9dff
thefx3 <fx.pairault@yahoo.fr>
2025-06-01 15:16:04 +0200

    Add styles.css

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log("Hello there");
+import "./styles.css";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const dropdown = document.createElement("select");
+  const options = ["Option 1", "Option 2", "Option 3"];
+
+  options.forEach((option) => {
+    const opt = document.createElement("option");
+    opt.value = option;
+    opt.textContent = option;
+    dropdown.appendChild(opt);
+  });
+
+  document.body.appendChild(dropdown);
+});
